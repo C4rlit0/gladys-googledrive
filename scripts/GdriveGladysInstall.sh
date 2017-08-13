45c047d71050057b7aef74a2b2a8ed996e92d7d4
C4rlit0 <charly.maillefaud@gmail.com>
2017-08-14 00:45:13 +0200

    First commit

---
+++ scripts/GdriveGladysInstall.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo '// INSTALLATION //'
+cd /home/pi/gladys/api/hooks/googledrive/scripts/
+echo "Ajout du droit d'execution sur le paquet gdrive et du script de sauvegarde"
+chmod +x gdrive
+chmod +x GdriveGladysBackup.sh
+echo 'Obtention du token Google Drive'
+./gdrive about
+
+echo '// INITIALISATION //'
+rm GLDS_FLD_ID.txt
+rm GLDS_BKP_FLD_ID.txt
+./gdrive mkdir Gladys | cut -d' ' -f2 > GLDS_FLD_ID.txt
+sleep 2
+var_GLDS_FLD_ID=$(cat GLDS_FLD_ID.txt)
+./gdrive mkdir -p "$var_GLDS_FLD_ID" GLDS_BDD_BKP | cut -d' ' -f2 > GLDS_BKP_FLD_ID.txt
+echo 'Arborescence de sauvegarde créée'
+echo 'Lancez votre premiere sauvegarde via ./GdriveGladysbackup.sh'

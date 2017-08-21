b2ed5ea689e9f58cb38fb1f37fe9703195974c8b
C4rlit0 <charly.maillefaud@gmail.com>
2017-08-22 00:06:18 +0200

    Update GdriveGladysBackup.sh
    Add clean-up fonction

--- scripts/GdriveGladysBackup.sh
+++ scripts/GdriveGladysBackup.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 echo '// SAUVEGARDE //'
 echo 'Création de la sauvegarde de la base de données'
+var_DAY_MAX="10" #durée max d’ancienneté des fichiers en jours
+var_DATE_OLD=$(date -d "$var_DAY_MAX day ago" +%Y-%m-%dT%H:%M:%S)
 var_BKP_FILE_NAME=gladys-$(date +%Y-%m-%d-%H-%M).sql
-mysqldump -uroot -proot gladys > /home/pi/gladys/api/hooks/googledrive/"$var_BKP_FILE_NAME"
+mysqldump -uroot -proot gladys > /home/pi/gladys-backup/"$var_BKP_FILE_NAME"
 echo 'Sauvegarde créée'
 echo 'Upload vers Google Drive'
-cd /home/pi/gladys/api/hooks/googledrive/
+cd /home/pi/gladys-backup
 var_GLDS_BKP_FLD_ID=$(cat GLDS_BKP_FLD_ID.txt)
 ./gdrive upload --parent "$var_GLDS_BKP_FLD_ID" "$var_BKP_FILE_NAME" --delete
 echo 'Sauvegarde terminée !'
+echo '// NETTOYAGE //'
+echo 'Création de la liste des anciens fichiers'
+./gdrive list --no-header -q "'$var_GLDS_BKP_FLD_ID' in parents and modifiedTime < '$var_DATE_OLD'" | cut -d' ' -f1 > GLDS_OLD_FILE.txt
+echo 'Suppression des anciens fichiers'
+for i in $(cat GLDS_OLD_FILE.txt)
+do
+./gdrive delete "$i"
+done
+echo 'Suppression de la liste'
+rm GLDS_OLD_FILE.txt
+echo 'Nettoyage terminé !' 
